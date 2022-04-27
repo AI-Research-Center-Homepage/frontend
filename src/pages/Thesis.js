89ac07c7c643a1fa13e83ab92731cc786f9f105a
baaaam771 <jeongiun@naver.com>
2022-04-28 02:04:36 +0900

    docs: Added vanilla version
    tab 기능 vanilla version 추가
    
    [#3]

--- src/pages/Thesis.js
+++ src/pages/Thesis.js
@@ -2,29 +2,17 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import SubHeader from "../components/SubHeader";
 
+import * as React from "react";
+import PropTypes from "prop-types";
 
-import * as React from 'react';
-import PropTypes from 'prop-types';
+import { AppBar, Tabs, Tab, Typography, Zoom, Fab, Box } from "@mui/material";
 
-import {
-  AppBar,
-  Tabs,
-  Tab,
-  Typography,
-  Zoom,
-  Fab,
-  Box
-} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import EditIcon from "@mui/icons-material/Edit";
+import UpIcon from "@mui/icons-material/KeyboardArrowUp";
 
-import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
-import UpIcon from '@mui/icons-material/KeyboardArrowUp';
-
-import { useTheme } from '@mui/material/styles';
-import { green } from '@mui/material/colors';
-
-
-
+import { useTheme } from "@mui/material/styles";
+import { green } from "@mui/material/colors";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -52,20 +40,20 @@ TabPanel.propTypes = {
 function a11yProps(index) {
   return {
     id: `action-tab-${index}`,
-    'aria-controls': `action-tabpanel-${index}`,
+    "aria-controls": `action-tabpanel-${index}`,
   };
 }
 
 const fabStyle = {
-  position: 'absolute',
+  position: "absolute",
   bottom: 16,
   right: 16,
 };
 
 const fabGreenStyle = {
-  color: 'common.white',
+  color: "common.white",
   bgcolor: green[500],
-  '&:hover': {
+  "&:hover": {
     bgcolor: green[600],
   },
 };
@@ -89,27 +77,26 @@ export default function FloatingActionButtonZoom() {
 
   const fabs = [
     {
-      color: 'primary',
+      color: "primary",
       sx: fabStyle,
       icon: <AddIcon />,
-      label: 'Add',
+      label: "Add",
     },
     {
-      color: 'secondary',
+      color: "secondary",
       sx: fabStyle,
       icon: <EditIcon />,
-      label: 'Edit',
+      label: "Edit",
     },
     {
-      color: 'inherit',
+      color: "inherit",
       sx: { ...fabStyle, ...fabGreenStyle },
       icon: <UpIcon />,
-      label: 'Expand',
+      label: "Expand",
     },
   ];
 
   return (
-    
     <div>
       <Header />
       <SubHeader main="소식" sub="논문" />
@@ -117,34 +104,33 @@ export default function FloatingActionButtonZoom() {
       <br />
       <br />
       <br />
-      
-    <Box
-      sx={{
-        bgcolor: 'background.paper',
-        width: 'auto',
-        position: 'relative',
-        minHeight: 200,
-        
-      }}
-    >
-      <AppBar position="static" color="default">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="fullWidth"
-          aria-label="action tabs example"
-        >
-          <Tab label="머신러닝" {...a11yProps(0)} />
-          <Tab label="컴퓨터 비전" {...a11yProps(1)} />
-          <Tab label="데이터 지능" {...a11yProps(2)} />
-          <Tab label="언어.음성" {...a11yProps(3)} />
-          <Tab label="로봇" {...a11yProps(4)} />
-          <Tab label="플랫폼" {...a11yProps(5)} />
-        </Tabs>
-      </AppBar>
-      {/* <SwipeableViews
+
+      <Box
+        sx={{
+          bgcolor: "background.paper",
+          width: "auto",
+          position: "relative",
+          minHeight: 200,
+        }}
+      >
+        <AppBar position="static" color="default">
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="fullWidth"
+            aria-label="action tabs example"
+          >
+            <Tab label="머신러닝" {...a11yProps(0)} />
+            <Tab label="컴퓨터 비전" {...a11yProps(1)} />
+            <Tab label="데이터 지능" {...a11yProps(2)} />
+            <Tab label="언어.음성" {...a11yProps(3)} />
+            <Tab label="로봇" {...a11yProps(4)} />
+            <Tab label="플랫폼" {...a11yProps(5)} />
+          </Tabs>
+        </AppBar>
+        {/* <SwipeableViews
         axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
         index={value}
         onChangeIndex={handleChangeIndex}
@@ -167,24 +153,26 @@ export default function FloatingActionButtonZoom() {
         <TabPanel value={value} index={5} dir={theme.direction}>
           FAST 2021 (USENIX 파일저장기술학회)
         </TabPanel>
-      {/* </SwipeableViews> */}
-      {fabs.map((fab, index) => (
-        <Zoom
-          key={fab.color}
-          in={value === index}
-          timeout={transitionDuration}
-          style={{
-            transitionDelay: `${value === index ? transitionDuration.exit : 0}ms`,
-          }}
-          unmountOnExit
-        >
-          <Fab sx={fab.sx} aria-label={fab.label} color={fab.color}>
-            {fab.icon}
-          </Fab>
-        </Zoom>
-      ))}
-    </Box>
-    <Footer />
+        {/* </SwipeableViews> */}
+        {fabs.map((fab, index) => (
+          <Zoom
+            key={fab.color}
+            in={value === index}
+            timeout={transitionDuration}
+            style={{
+              transitionDelay: `${
+                value === index ? transitionDuration.exit : 0
+              }ms`,
+            }}
+            unmountOnExit
+          >
+            <Fab sx={fab.sx} aria-label={fab.label} color={fab.color}>
+              {fab.icon}
+            </Fab>
+          </Zoom>
+        ))}
+      </Box>
+      <Footer />
     </div>
   );
 }
