50290e45dfd62436535859168c774295d22427e9
LimEunSang <dmstkd2905@naver.com>
2022-05-02 23:49:29 +0900

    feat: Create Graduate page
    구성원 -> 대학원생 페이지를 구현했습니다.
    데이터베이스에 연동할 대학원생 정보는 dummydate로 임시 저장했습니다.
    현재 날짜의 년도를 받아와 랜더링 했습니다. 년도가 바뀔때마다 자동
    갱신됩니다.
    선택된 년도에 해당한 대학원생만 랜더링하기 위해 filter 함수를 적용했습니다.
    저장될 사진의 크기를 확정할 수 없어 높이를 맞추기 위해 임시 코드
    작성했습니다. 후에 수정이 필요합니다.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { createContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Graduate from "./pages/member/Graduate";
 
 export const ArticleContext = createContext();
 
@@ -11,6 +12,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/graduate" element={<Graduate />} />
         </Routes>
       </BrowserRouter>
     </ArticleContext.Provider>
