9de440963def0202717c77e7a2051c100f4a3590
euny0ung <hdtv0218@gmail.com>
2022-05-11 00:35:15 +0900

    fix: Update Prof 이미지 경로 오류를 수정하였습니다. - 기존 이미지 삭제 후 랜덤으로 이미지를 보여주는 사이트 사용 #20

--- src/pages/member/Prof.js
+++ src/pages/member/Prof.js
@@ -4,7 +4,6 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-// import baby from "../assets/images/baby.png";
 
 import Header from "../../components/Header";
 import SubHeader from "../../components/SubHeader";
@@ -20,7 +19,7 @@ const prof = [
     email: "cheetah@jbnu.ac.kr",
     lab: "101호",
     number: "010-1111-1111",
-    // img: { baby },
+    img: "https://images.unsplash.com/photo-1645148100502-418e89bf99c2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MjEwNzU1Mw&ixlib=rb-1.2.1&q=80&w=1080",
   },
   {
     name: "치타리",
@@ -30,7 +29,7 @@ const prof = [
     email: "cheetahLee@jbnu.ac.kr",
     lab: "202호",
     number: "010-1111-1111",
-    // img: { baby },
+    img: "https://images.unsplash.com/photo-1645148100502-418e89bf99c2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MjEwNzU1Mw&ixlib=rb-1.2.1&q=80&w=1080",
   },
   {
     name: "무민",
@@ -40,7 +39,7 @@ const prof = [
     email: "moomin@jbnu.ac.kr",
     lab: "203호",
     number: "010-1111-1111",
-    // img: { baby },
+    img: "https://images.unsplash.com/photo-1645148100502-418e89bf99c2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MjEwNzU1Mw&ixlib=rb-1.2.1&q=80&w=1080",
   },
   {
     name: "에펠탑",
@@ -50,7 +49,7 @@ const prof = [
     email: "paris@jbnu.ac.kr",
     lab: "101호",
     number: "010-1111-1111",
-    // img: { baby },
+    img: "https://images.unsplash.com/photo-1645148100502-418e89bf99c2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MjEwNzU1Mw&ixlib=rb-1.2.1&q=80&w=1080",
   },
   {
     name: "냉면",
@@ -60,7 +59,7 @@ const prof = [
     email: "good@jbnu.ac.kr",
     lab: "301호",
     number: "010-1111-1111",
-    // img: { baby },
+    img: "https://images.unsplash.com/photo-1645148100502-418e89bf99c2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MjEwNzU1Mw&ixlib=rb-1.2.1&q=80&w=1080",
   },
 ];
 
@@ -80,18 +79,24 @@ export default function Prof() {
             display: "flex",
             width: "50%",
             marginTop: "1%",
+            marginBottom: "1%",
             marginLeft: "25%",
           }}
         >
           {/* 교수 사진 */}
           <CardMedia
             component="img"
-            sx={{ width: 200, padding: "2%" }}
-            // image={baby}
+            sx={{ width: "20%", height: "20%", padding: "2%" }}
+            image={element.img}
             alt="image"
           />
           {/* 텍스트 정보 */}
-          <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
             <CardContent sx={{ flex: "1 0 auto" }}>
               <Typography component="div" variant="subtitle2">
                 <strong>{element.name}</strong>
