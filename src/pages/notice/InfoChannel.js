3010c63921c4b8551186a3cfececd9dd7603cd27
baaaam771 <jeongiun@naver.com>
2022-05-11 17:03:07 +0900

    chore: Delete unused code
    사용 하지 않는 theme 관련 코드 제거

--- src/pages/notice/InfoChannel.js
+++ src/pages/notice/InfoChannel.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 
 import { CssBaseline, Grid, Container } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import FeaturedPost from "../../components/FeaturedPost";
 import Header from "../../components/Header";
@@ -82,13 +81,10 @@ const info_data = [
  *    FeaturedPost 컴포넌트 불러서 info data 화면에 뿌려주기
  */
 
-const theme = createTheme();
-
 export default function Post() {
   return (
-    <ThemeProvider theme={theme}>
+    <div>
       <CssBaseline />
-
       <Header />
       <SubHeader main="소식" sub="소식통" />
       {/* 정렬 위아래 padding 너비 auto에 최대너비 고정 */}
@@ -105,8 +101,7 @@ export default function Post() {
           ))}
         </Grid>
       </Container>
-
       <Footer />
-    </ThemeProvider>
+    </div>
   );
 }
