b8158d8118a9dce52d95f124ebf95a9f83bca5db
JSI <suin8@jbnu.ac.kr>
2022-05-11 19:10:40 +0900

    refactor: Refactoring Header-Side Menu
    sideMenu의 Collapse가 정상적으로 작동하지 않는 것 수정하였습니다.
    - 함수화하여 따로 넣어줌 => 코드 전체 MainMenu에 삽입
    - 이전 코드는 이슈확인을 위해 주석처리하여 남겨놓았습니다.
    
    [#35]

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,20 +13,20 @@ import {
   useMediaQuery,
   Typography,
   Drawer,
-  AccordionSummary,
-  AccordionDetails,
-  Accordion,
+  List,
+  ListItemButton,
+  ListItemText,
   Stack,
   Collapse,
 } from "@mui/material";
 import { Home } from "@mui/icons-material";
 import MenuIcon from "@mui/icons-material/Menu";
-import "../App.css";
+
 import jbnu from "../assets/images/jbnu.png";
 
 /**
  *@author Suin-Jeong, suin8@jbnu.ac.kr
- *@date 2022-04-13
+ *@date 2022-05-11
  *@description 상단에 고정적으로 위치하는 Header
  *             로고, Main Navigator, 검색창,
  *             KR/EN 버튼, Side Navigator 포함
@@ -34,29 +34,49 @@ import jbnu from "../assets/images/jbnu.png";
 
 const mainMenuItems = [
   {
-    id: 1,
+    key: 1,
     title: "소개",
-    contents: ["연구센터소개", "조직도", "연혁", "센터위치"],
+    contents: [
+      { subkey: 6, subcontent: "연구센터소개" },
+      { subkey: 7, subcontent: "조직도" },
+      { subkey: 8, subcontent: "연혁" },
+      { subkey: 9, subcontent: "센터위치" },
+    ],
   },
   {
-    id: 2,
+    key: 2,
     title: "연구",
-    contents: ["AI연구분야", "연구프로젝트", "연구논문", "연구결과데모"],
+    contents: [
+      { subkey: 10, subcontent: "AI연구분야" },
+      { subkey: 11, subcontent: "연구프로젝트" },
+      { subkey: 12, subcontent: "연구논문" },
+      { subkey: 13, subcontent: "연구결과데모" },
+    ],
   },
   {
-    id: 3,
+    key: 3,
     title: "구성원",
-    contents: ["참여교수", "운영위원회", "대학원생", "학부 연구원생", "연구원"],
+    contents: [
+      { subkey: 14, subcontent: "참여교수" },
+      { subkey: 15, subcontent: "운영위원회" },
+      { subkey: 16, subcontent: "대학원생" },
+      { subkey: 17, subcontent: "학부 연구원생" },
+      { subkey: 18, subcontent: "연구원" },
+    ],
   },
   {
-    id: 4,
+    key: 4,
     title: "소식",
-    contents: ["소식통", "공지사항", "언론보도"],
+    contents: [
+      { subkey: 19, subcontent: "소식통" },
+      { subkey: 20, subcontent: "공지사항" },
+      { subkey: 21, subcontent: "언론보도" },
+    ],
   },
   {
-    id: 5,
+    key: 5,
     title: "지원하기",
-    contents: ["지원하기"],
+    contents: [{ subkey: 22, subcontent: "지원하기" }],
   },
 ];
 
@@ -64,76 +84,89 @@ const Header = () => {
   const mainMenuMediaQuery = useMediaQuery("(min-width: 1200px)");
 
   const [sideMenuOpen, setSideMenuOpen] = useState(false);
-
   const [collapseMenuOpen, setCollapseMenuOpen] = useState(false);
+  const [expandedSideMenu, setExpandedSideMenu] = useState(false);
 
-  const navigate = useNavigate();
+  const handleExpanded = (key) => {
+    if (expandedSideMenu === key) {
+      setExpandedSideMenu("");
+    } else {
+      setExpandedSideMenu(key);
+    }
+  };
 
   const isShowMainMenu = () => {
     return mainMenuMediaQuery ? "visible" : "hidden";
   };
 
+  const navigate = useNavigate();
+
   /**
    *@author Suin-Jeong, suin8@jbnu.ac.kr
-   *@date 2022-04-13
+   *@date 2022-05-11
    *@description 오른쪽 사이드 메뉴
-   *             Accordion을 이용하여 구현
+   *             list를 이용하여 구현
    */
 
-  const SideMenu = () => {
-    return (
-      <Card
-        sx={{
-          height: "100%",
-          width: "320px",
-          bgcolor: "#0277BD",
-          color: "white",
-          borderRadius: "0",
-        }}
-      >
-        <CardMedia sx={{ ml: "7px", my: "7px" }}>
-          <IconButton onClick={() => navigate("/")}>
-            <Home sx={{ color: "white" }} />
-          </IconButton>
-        </CardMedia>
-        {mainMenuItems.map((it) => (
-          <Accordion
-            sx={{
-              bgcolor: "#0277BD",
-              color: "white",
-            }}
-          >
-            <AccordionSummary
-              aria-controls={`content${it.title}`}
-              id={`content${it.title}-header`}
-            >
-              <Typography variant="subtitle1">{it.title}</Typography>
-            </AccordionSummary>
-            {it.contents.map((content) => (
-              <AccordionDetails
-                sx={{ bgcolor: "white", color: "black", pb: "8px" }}
-              >
-                <Button
-                  variant="text"
-                  sx={{
-                    color: "black",
-                    justifyContent: "left",
-                  }}
-                  onClick={() => {}}
-                >
-                  {content}
-                </Button>
-              </AccordionDetails>
-            ))}
-          </Accordion>
-        ))}
-      </Card>
-    );
-  };
+  // 함수화 시켜서 사용하면 transition 기능이 사라짐
+  // Why?????
+
+  // const SideMenu = () => {
+  //   return (
+  //     <>
+  //       <Card
+  //         sx={{
+  //           height: "100%",
+  //           width: "320px",
+  //           bgcolor: "#0277BD",
+  //           color: "white",
+  //           borderRadius: "0",
+  //         }}
+  //       >
+  //         <CardMedia sx={{ ml: "7px", my: "7px" }}>
+  //           <IconButton onClick={() => navigate("/")}>
+  //             <Home sx={{ color: "white" }} />
+  //           </IconButton>
+  //         </CardMedia>
+
+  //         <List sx={{ width: "100%", maxWidth: 360, bgcolor: "#0277BD" }}>
+  //           {mainMenuItems.map(({ key, title, contents }) => (
+  //             <div key={key}>
+  //               <ListItemButton onClick={() => handleExpanded(key)}>
+  //                 <ListItemText primary={title} />
+  //               </ListItemButton>
+
+  //               <Collapse
+  //                 in={key === expandedSideMenu}
+  //                 timeout="auto"
+  //                 unmountOnExit
+  //               >
+  //                 <List
+  //                   component="div"
+  //                   disablePadding
+  //                   sx={{ bgcolor: "white" }}
+  //                 >
+  //                   {contents.map(({ subkey, subcontent }) => (
+  //                     <ListItemButton
+  //                       sx={{ pl: 4, color: "black" }}
+  //                       key={subkey}
+  //                     >
+  //                       <ListItemText primary={subcontent} />
+  //                     </ListItemButton>
+  //                   ))}
+  //                 </List>
+  //               </Collapse>
+  //             </div>
+  //           ))}
+  //         </List>
+  //       </Card>
+  //     </>
+  //   );
+  // };
 
   /**
    *@author Suin-Jeong, suin8@jbnu.ac.kr
-   *@date 2022-04-13
+   *@date 2022-05-11
    *@description 상단 헤더에서 MainMenu 구성부
    *             onMouse시 collapseMenu가 내려옴
    */
@@ -141,10 +174,10 @@ const Header = () => {
   const MainMenu = () => {
     return (
       <>
-        {mainMenuItems.map((it) => (
-          <Grid item xs={2.4}>
+        {mainMenuItems.map(({ title, key }) => (
+          <Grid item xs={2.4} key={key}>
             <Typography sx={{ fontSize: "20px", color: "black" }}>
-              {it.title}
+              {title}
             </Typography>
           </Grid>
         ))}
@@ -154,7 +187,7 @@ const Header = () => {
 
   /**
    *@author Suin-Jeong, suin8@jbnu.ac.kr
-   *@date 2022-04-13
+   *@date 2022-05-11
    *@description Header컴포넌트 전체 구성부
    *             헤더를 Grid로(총 12) 세분화
    *             (1 Logo , 10, 1 SideMenuIcon)
@@ -272,14 +305,61 @@ const Header = () => {
               anchor="right"
               open={sideMenuOpen}
               onClose={() => setSideMenuOpen(false)}
-              onOpen={() => {}}
             >
-              <SideMenu />
+              {/* SideMenu Collapse */}
+              <Card
+                sx={{
+                  height: "100%",
+                  width: "320px",
+                  bgcolor: "#0277BD",
+                  color: "white",
+                  borderRadius: "0",
+                }}
+              >
+                <CardMedia sx={{ ml: "7px", mt: "7px" }}>
+                  <IconButton onClick={() => navigate("/")}>
+                    <Home sx={{ color: "white" }} />
+                  </IconButton>
+                </CardMedia>
+
+                <List sx={{ width: "100%", maxWidth: 360, bgcolor: "#0277BD" }}>
+                  {mainMenuItems.map(({ key, title, contents }) => (
+                    <div key={key}>
+                      <ListItemButton
+                        sx={{ py: 1.5 }}
+                        onClick={() => handleExpanded(key)}
+                      >
+                        <ListItemText primary={title} />
+                      </ListItemButton>
+
+                      <Collapse
+                        in={key === expandedSideMenu}
+                        timeout="auto"
+                        unmountOnExit
+                      >
+                        <List
+                          component="div"
+                          disablePadding
+                          sx={{ bgcolor: "white" }}
+                        >
+                          {contents.map(({ subkey, subcontent }) => (
+                            <ListItemButton
+                              sx={{ pl: 4, color: "black" }}
+                              key={subkey}
+                            >
+                              <ListItemText primary={subcontent} />
+                            </ListItemButton>
+                          ))}
+                        </List>
+                      </Collapse>
+                    </div>
+                  ))}
+                </List>
+              </Card>
             </Drawer>
           </Grid>
         </Grid>
       </Box>
-
       <Divider />
 
       {/* collapse 메뉴 */}
@@ -297,14 +377,16 @@ const Header = () => {
           <Grid item xs={1} />
           <Grid container xs={10}>
             <Grid container xs={8}>
-              {mainMenuItems.map((it) => (
+              {mainMenuItems.map(({ contents }) => (
                 <>
                   <Divider orientation="vertical" />
                   <Grid item xs>
                     <Stack sx={{ height: "100%" }}>
-                      {it.contents.map((content) => (
+                      {contents.map(({ subcontent }) => (
                         <Button sx={{ height: "20%", color: "black" }}>
-                          <Typography variant="subtitle2">{content}</Typography>
+                          <Typography variant="subtitle2">
+                            {subcontent}
+                          </Typography>
                         </Button>
                       ))}
                     </Stack>
