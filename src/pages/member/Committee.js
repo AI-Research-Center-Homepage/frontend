b2447c87ee0e03a60520b1e463e2227f7f04d5e1
baaaam771 <jeongiun@naver.com>
2022-05-19 15:33:16 +0900

    fix: useState member variable error
    - member 변수 p 태그로 return 가능
    - member.map에서 오류
    
    type error [#41]

--- src/pages/member/Committee.js
+++ src/pages/member/Committee.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { CssBaseline, Grid, Container } from "@mui/material";
-// import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import FeaturedPost from "../../components/FeaturedPost";
 import Header from "../../components/Header";
@@ -8,7 +7,7 @@ import SubHeader from "../../components/SubHeader";
 import Footer from "../../components/Footer";
 
 import axios from "axios";
-import api from "../../mocks/api";
+// import api from "../../mocks/api";
 import { useEffect, useState } from "react";
 
 /*
@@ -83,9 +82,9 @@ const committee = [
 // console.log(result.data);
 
 // api.getCommittee();
-const _results = api.getCommittee();
+// const _results = api.getCommittee();
 // const result = _results.data;
-console.log(_results);
+// console.log(_results);
 
 // 첫번째 버전
 // axios({
@@ -171,27 +170,24 @@ console.log(_results);
  *    FeaturedPost 컴포넌트 불러서 committee data 화면에 뿌려주기
  */
 
-// const theme = createTheme();
-
 export default function Post() {
-  const [list, setList] = useState([]);
+  const [member, setMember] = useState([]);
   useEffect(() => {
     axios
       .get(
         "https://97039e2f-9785-4469-a9c2-3b173ce13447.mock.pstmn.io/list/committee"
       )
       .then((response) => {
-        setList(response.data);
+        setMember(response.data);
       });
   }, []);
   return (
-    // <ThemeProvider theme={theme}>
     <div>
       <CssBaseline />
       <Header />
       <SubHeader main="구성원" sub="운영위원회" />
       {/* 정렬 위아래 padding 너비 auto에 최대너비 고정 */}
-      {/* <p>{datas}</p> */}
+      <p>{member}</p>
       <Container
         sx={{
           py: 8,
@@ -200,13 +196,13 @@ export default function Post() {
         maxWidth="md"
       >
         <Grid container spacing={4}>
-          {list.map((post) => (
-            <FeaturedPost post={post} />
+          {/* member.map으로 변경하면 오류 */}
+          {committee.map((card) => (
+            <FeaturedPost post={card} />
           ))}
         </Grid>
       </Container>
       <Footer />
     </div>
-    // {/* </ThemeProvider> */}
   );
 }
