c726d5e91065c148e0f84acfe2c8f44ccbbb6487
baaaam771 <jeongiun@naver.com>
2022-06-22 14:21:53 +0900

    fix: Restore feature post component & delete comment
    - FeaturedPost component navigate 기능 복구
    - json data 표기 주석 제거

--- src/pages/news/Article.js
+++ src/pages/news/Article.js
@@ -13,44 +13,6 @@ import axios from "axios";
 /**
  *@author BeomGi-Lee jeongiun@naver.com
  *@date 2022-05-08
- *@name article
- *@description
- *    언론보도 dummy data
- */
-// const dummyCards = [
-//   {
-//     id: 1,
-//     title: "1번 컨텐츠",
-//     image: "https://source.unsplash.com/random",
-//     createdDate: "2022-01-01'T'00:00:00",
-//     modifiedDate: "2022-01-02'T'00:00:00",
-//   },
-//   {
-//     id: 2,
-//     title: "2번 컨텐츠",
-//     image: "https://source.unsplash.com/random",
-//     createdDate: "2022-01-03'T'00:00:00",
-//     modifiedDate: "2022-01-04'T'00:00:00",
-//   },
-//   {
-//     id: 3,
-//     title: "3번 컨텐츠",
-//     image: "https://source.unsplash.com/random",
-//     createdDate: "2022-01-05'T'00:00:00",
-//     modifiedDate: "2022-01-06'T'00:00:00",
-//   },
-//   {
-//     id: 4,
-//     title: "4번 컨텐츠",
-//     image: "https://source.unsplash.com/random",
-//     createdDate: "2022-01-07'T'00:00:00",
-//     modifiedDate: "2022-01-08'T'00:00:00",
-//   },
-// ];
-
-/**
- *@author BeomGi-Lee jeongiun@naver.com
- *@date 2022-05-08
  *@name Post
  *@description
  *    FeaturedPost 컴포넌트 불러서 article data 화면에 뿌려주기
