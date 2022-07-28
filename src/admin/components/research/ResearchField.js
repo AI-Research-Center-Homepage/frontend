f9fd5995f8492c94f1d2f8354f34ab896850d0f0
euny0ung <hdtv0218@gmail.com>
2022-07-28 18:18:18 +0900

    feat: Add new field - 새 연구분야를 추가하는 창

--- src/admin/components/research/ResearchField.js
+++ src/admin/components/research/ResearchField.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 import {
   Card,
   CardContent,
@@ -7,28 +7,29 @@ import {
   Button,
   Stack,
   Box,
-} from "@mui/material";
+  TextField,
+} from '@mui/material';
 
 const Field = [
   {
-    fieldName: "연구분야1",
-    description: "설명1",
+    fieldName: '연구분야1',
+    description: '설명1',
   },
   {
-    fieldName: "연구분야2",
-    description: "설명2",
+    fieldName: '연구분야2',
+    description: '설명2',
   },
   {
-    fieldName: "연구분야3",
-    description: "설명3",
+    fieldName: '연구분야3',
+    description: '설명3',
   },
   {
-    fieldName: "연구분야4",
-    description: "설명4",
+    fieldName: '연구분야4',
+    description: '설명4',
   },
   {
-    fieldName: "연구분야5",
-    description: "설명5",
+    fieldName: '연구분야5',
+    description: '설명5',
   },
 ];
 
@@ -39,18 +40,18 @@ const ResearchField = () => {
         <Grid item xs={12}>
           {Field.map((element) => (
             <Box>
-              <div style={{ display: "flex", justifyContent: "center" }}>
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <Card
                   sx={{
-                    display: "flex",
+                    display: 'flex',
                     boxShadow: 5,
                     my: 1,
-                    width: { xs: "70%", md: "60%" },
+                    width: { xs: '70%', md: '60%' },
                     maxHeight: 200,
                   }}
                 >
                   {/* 텍스트 정보 */}
-                  <CardContent sx={{ flex: "1 0 auto" }}>
+                  <CardContent sx={{ flex: '1 0 auto' }}>
                     <Typography component="div" variant="body1">
                       <strong>{element.fieldName}</strong>
                     </Typography>
@@ -65,6 +66,25 @@ const ResearchField = () => {
               </div>
             </Box>
           ))}
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <TextField
+              id="outlined-multiline-static"
+              label="연구분야"
+              multiline
+              rows={2}
+              sx={{ mt: 2, width: { md: '15%', xs: '25%' } }}
+            />
+            <TextField
+              id="outlined-multiline-static"
+              label="설명"
+              multiline
+              rows={2}
+              sx={{ mt: 2, ml: 2, width: { md: '45%', xs: '40%' } }}
+            />
+            <Button variant="contained" sx={{ height: '100%', mt: 4, ml: 1 }}>
+              저장
+            </Button>
+          </div>
         </Grid>
       </Grid>
     </div>
