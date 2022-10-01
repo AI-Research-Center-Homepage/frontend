a5281850efa349b8d96d53a81fbfd96e5caebba3
JSI <suin8@jbnu.ac.kr>
2022-10-01 22:02:33 +0900

    feat: connect to backend
    - 백엔드 연결에 필요한 세팅

--- src/admin/components/CommonButton.js
+++ src/admin/components/CommonButton.js
@@ -2,11 +2,9 @@ import { Button } from "@mui/material";
 
 const CommonButton = ({ content, onClick, sx }) => {
   return (
-    <>
-      <Button variant="contained" size="large" onClick={onClick} sx={sx}>
-        {content}
-      </Button>
-    </>
+    <Button variant="contained" size="large" onClick={onClick} sx={sx}>
+      {content}
+    </Button>
   );
 };
 
--- src/user/pages/member/Committee.js
+++ src/user/pages/member/Committee.js
@@ -9,62 +9,9 @@ import Header from "../../components/Header";
 import SubHeader from "../../components/SubHeader";
 import Footer from "../../components/Footer";
 
-/*
+/**
  *@author BeomGi-Lee jeongiun@naver.com
- *@date 2022-05-04
- *@name committee
- *@description
- *    운영위원회 dummy data
- */
-
-// const committee = [
-//   {
-//     position: "원장",
-//     name: "김원장",
-//     major: "공과대학 컴퓨터 공학부",
-//     image:
-//       "https://w.namu.la/s/379ef8ae4f12fc04481c2cb7a97c2bdc901727b137c38fd652cc7aea80f74e56185fa0fd62658554fc3e6e79861aec73782db1d7ab3943275607e6623a46d09fe63412410041cd4b2b6ea4e740e6a290c8022da99c4d567b5533c100da0ed814",
-//   },
-//   {
-//     position: "연구부원장",
-//     name: "박부원장",
-//     major: "공과대학 컴퓨터 공학부",
-//     image:
-//       "https://w.namu.la/s/379ef8ae4f12fc04481c2cb7a97c2bdc901727b137c38fd652cc7aea80f74e56185fa0fd62658554fc3e6e79861aec73782db1d7ab3943275607e6623a46d09fe63412410041cd4b2b6ea4e740e6a290c8022da99c4d567b5533c100da0ed814",
-//   },
-//   {
-//     position: "기획부원장",
-//     name: "이부원장",
-//     major: "공과대학 컴퓨터 공학부",
-//     image:
-//       "https://w.namu.la/s/379ef8ae4f12fc04481c2cb7a97c2bdc901727b137c38fd652cc7aea80f74e56185fa0fd62658554fc3e6e79861aec73782db1d7ab3943275607e6623a46d09fe63412410041cd4b2b6ea4e740e6a290c8022da99c4d567b5533c100da0ed814",
-//   },
-//   {
-//     position: "플랫폼기술부장",
-//     name: "최부장",
-//     major: "공과대학 컴퓨터 공학부",
-//     image:
-//       "https://w.namu.la/s/379ef8ae4f12fc04481c2cb7a97c2bdc901727b137c38fd652cc7aea80f74e56185fa0fd62658554fc3e6e79861aec73782db1d7ab3943275607e6623a46d09fe63412410041cd4b2b6ea4e740e6a290c8022da99c4d567b5533c100da0ed814",
-//   },
-//   {
-//     position: "응용기술부장",
-//     name: "정부장",
-//     major: "공과대학 컴퓨터 공학부",
-//     image:
-//       "https://w.namu.la/s/379ef8ae4f12fc04481c2cb7a97c2bdc901727b137c38fd652cc7aea80f74e56185fa0fd62658554fc3e6e79861aec73782db1d7ab3943275607e6623a46d09fe63412410041cd4b2b6ea4e740e6a290c8022da99c4d567b5533c100da0ed814",
-//   },
-//   {
-//     position: "기획대외협력부장",
-//     name: "부부장",
-//     major: "공과대학 컴퓨터 공학부",
-//     image:
-//       "https://w.namu.la/s/379ef8ae4f12fc04481c2cb7a97c2bdc901727b137c38fd652cc7aea80f74e56185fa0fd62658554fc3e6e79861aec73782db1d7ab3943275607e6623a46d09fe63412410041cd4b2b6ea4e740e6a290c8022da99c4d567b5533c100da0ed814",
-//   },
-// ];
-
-/*
- *@author BeomGi-Lee jeongiun@naver.com
- *@date 2022-07-15
+ *@date 2022-10-01
  *@name Post
  *@description
  *    FeaturedPost 컴포넌트 불러서 committee data 화면에 뿌려주기
@@ -75,7 +22,8 @@ export default function Post() {
   useEffect(() => {
     axios
       .get(
-        "https://97039e2f-9785-4469-a9c2-3b173ce13447.mock.pstmn.io/list/committee2"
+        //"https://97039e2f-9785-4469-a9c2-3b173ce13447.mock.pstmn.io/list/committee2"
+        "/api/committee"
       )
       .then((response) => {
         setCommittee(response.data);
